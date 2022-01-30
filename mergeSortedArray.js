160ffb9a9231169356d6b7e02ba6e7da9240e09b
Hendson <hendsonszeto@live.com>
2022-01-29 18:32:36 -0800

    Update merge sort array

--- mergeSortedArray.js
+++ mergeSortedArray.js
@@ -5,17 +5,22 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
+// Needs work
  var merge = function(nums1, m, nums2, n) {
     let i = 0;
     let j = 0;
     
     while (j <= n - 1) {
-        if (nums2[j] <= nums1[i]) {
+        if (m === 0) {
+            nums1.splice(i, 0, nums2[j]);
+            nums1.pop();
+            j++;
+        } else if (nums2[j] <= nums1[i]) {
             nums1.splice(i, 0, nums2[j]);
             nums1.pop();
             i++;
             j++;
-        } else if (nums1[i] === 0) {
+        } else if (nums1[i] === 0 && i > m) {
             nums1.splice(i, 0, nums2[j]);
             nums1.pop();
             j++;
@@ -24,8 +29,9 @@
         }
     }
 
+    console.log(nums1);
     return nums1;
 };
 
 
-merge([1,2,3,0,0,0], 3, [2,5,6], 3);
+merge([1,0],1,[2],1);
