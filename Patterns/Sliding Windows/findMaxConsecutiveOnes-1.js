3b279df3510363842c13e3d7b6cd393c0c763a20
Hendson <hendsonszeto@live.com>
2022-02-01 12:35:21 -0800

    Final solution to findMaxConsecutiveOnes-1

--- Patterns/Sliding Windows/findMaxConsecutiveOnes-1.js
+++ Patterns/Sliding Windows/findMaxConsecutiveOnes-1.js
@@ -2,28 +2,31 @@
  * @param {number[]} nums
  * @return {number}
  */
- var findMaxConsecutiveOnes = function(nums) {
+var findMaxConsecutiveOnes = function(nums) {
     let left = 0;
     let right = 0;
-    let maxConsecutiveOnes = 0;
+    let maxOnes = 0;
     
-    while (right < nums.length) {
-        if (nums[right] === 0) {
-            maxConsecutiveOnes = Math.max(maxConsecutiveOnes, right - left - 1);
-            left++;
+    while (right < nums.length) {                                   // Iterate through array
+        if (nums[right] === 0) {                                    // Condition to stop expansion
+            maxOnes = Math.max(maxOnes, right - left);              // Process subarray
+            left = right + 1;                                       // Contract window (yes, it is contracting even though it "looks" like your just setting left border to be greater than right border)
         }
-        right++;
+        right++;                                                    // Expand window
     }
-    maxConsecutiveOnes = Math.max(maxConsecutiveOnes, right - left - 1);
-    return maxConsecutiveOnes;
+
+    maxOnes = Math.max(maxOnes, right - left);                      // Handles edge cases where subarray doesn't end in 0
+    return maxOnes;                                                 
 };
 
-findMaxConsecutiveOnes([1,0,1,1,1,1,0])
+findMaxConsecutiveOnes([0,1])
 
 
 // Test Cases
-// [1,0,1,1,1,1,0]
+// [1,1,0,1,1,1]
 // [1,0,1,1,0,1]
 // [0,1]
 // [1,0]
 // [1,0,1,1]
+// [1,1,1]
+// [0,0,0]
