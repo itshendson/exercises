65925d12d9f89eba339962c71a218539d698366a
Hendson <hendsonszeto@live.com>
2022-01-31 17:14:36 -0800

    Create findMaxConsecutiveOnes file

---
+++ Patterns/Sliding Windows/findMaxConsecutiveOnes.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var findMaxConsecutiveOnes = function(nums) {
+    let left = 0;
+    let right = 0;
+    let countOfZeroes = 0;
+    let maxConsecutiveOnes = 0;
+    
+    while (right < nums.length) {
+        
+        if (nums[right] === 0) countOfZeroes++
+        
+        while (countOfZeroes >= 2) {
+            maxConsecutiveOnes = Math.max(maxConsecutiveOnes, right - left);
+            console.log(nums[left])
+            if (nums[left] = 0) countOfZeroes--;
+            left++;
+        }
+        
+        right++;
+    }
+    
+    return maxConsecutiveOnes;
+};
+
+findMaxConsecutiveOnes([0,1,0]);
