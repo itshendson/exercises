2c4f8b139cce4e10371ad9e553041da60698b839
Hendson <hendsonszeto@live.com>
2022-02-12 16:34:50 -0800

    Two pointer solution to validPalindrome

--- Patterns/Two Pointers/validPalindrome.js
+++ Patterns/Two Pointers/validPalindrome.js
@@ -2,7 +2,25 @@
  * @param {string} s
  * @return {boolean}
  */
- var isPalindrome = function(s) {
+ var isPalindrome = function(s) {   
+    s = s.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        if (s[left] !== s[right]) return false;
+        left++;
+        right--;
+    }
+    return true;
+};
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ * First method, very slow...apparently
+ */
+/* var isPalindromeOldMethod = function(s) {
     let alphanumeric = ['1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
     let sLowerCase = s.toLowerCase();
     let str = "";
@@ -14,14 +32,15 @@
     let reversed = str.split('').reverse().join('');
     let result = str === reversed ? true : false;
     return result
-};
+}; */
 
-isPalindrome("+");
+isPalindrome("0P");
 
 /* 
  * Test Case
  * "121"
  * "A man, a plan, a canal: Panama"
+ * "A,boba"
  * "race a car"
  * " "
  * "+"
